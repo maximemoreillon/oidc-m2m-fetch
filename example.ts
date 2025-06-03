b182192254df3f5db46a1e6ade1ec749dd149aa7
Maxime Moreillon <m.moreillon@gmail.com>
2025-06-03 13:15:54 +0900

    minor

--- example.ts
+++ example.ts
@@ -1,7 +1,5 @@
-import dotenv from "dotenv"
-import Client from "."
-
-dotenv.config()
+import "dotenv/config";
+import Client from ".";
 
 const {
   OIDC_CLIENT_ID,
@@ -9,21 +7,21 @@ const {
   OIDC_TOKEN_URL,
   OIDC_AUDIENCE,
   TARGET_ENDPOINT_URL = "http://localhost:8080",
-} = process.env
+} = process.env;
 
 if (!OIDC_CLIENT_ID || !OIDC_CLIENT_SECRET || !OIDC_TOKEN_URL)
-  throw new Error("Misconfigured auth")
+  throw new Error("Misconfigured auth");
 
 const client = new Client({
   client_id: OIDC_CLIENT_ID,
   client_secret: OIDC_CLIENT_SECRET,
   token_url: OIDC_TOKEN_URL,
   audience: OIDC_AUDIENCE,
-})
+});
 
 async function main() {
-  const response = await client.fetch(TARGET_ENDPOINT_URL)
-  console.log(response.ok)
+  const response = await client.fetch(TARGET_ENDPOINT_URL);
+  console.log(response.ok);
 }
 
-main()
+main();
